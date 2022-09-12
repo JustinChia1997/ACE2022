aef0e483936d1ddd8340fd786fad5963530bed47
JustinChia1997 <justinchiajj@hotmail.com>
2022-09-13 00:48:23 +0800

    Included logic for sending data back to app.js for processing

--- championship/src/App.js
+++ championship/src/App.js
@@ -2,11 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import TeamInput from './components/teamInput.js';
+import { useState } from 'react';
 
 function App() {
+  const [teamData, setTeamData] = useState(0);
+
+  const handleTeamInput = input => {
+    setTeamData(input);
+    console.log('team input received')
+    console.log(input)
+  }
+
   return (
     <div className="App">
-      <TeamInput></TeamInput>
+      <TeamInput handleTeamInput={handleTeamInput}></TeamInput>
     </div>
   );
 }
--- championship/src/components/teamInput.js
+++ championship/src/components/teamInput.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import Form from 'react-bootstrap/Form';
 
-function TeamInput() {
+function TeamInput(props) {
+    const onInput = ({target:{value}}) => { 
+        props.handleTeamInput(value);
+        console.log('value changing');
+    }
     return (
     <div>
         <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-            <Form.Label>Example textarea</Form.Label>
-            <Form.Control as="textarea" rows={12} width={300} />
+            <Form.Label>Team Information</Form.Label>
+            <Form.Control as="textarea" rows={12} width={300} onChange={onInput} />
       </Form.Group>
     </div>
     )
