5fda0ed385aae67628edc302e9d19f2c2dd7cff6
JustinChia1997 <justinchiajj@hotmail.com>
2022-09-13 00:27:32 +0800

    installed redux for global state access

--- championship/src/App.js
+++ championship/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import TeamInput from './components/teamInput.js';
 
 function App() {
   return (
     <div className="App">
+      <TeamInput></TeamInput>
     </div>
   );
 }
